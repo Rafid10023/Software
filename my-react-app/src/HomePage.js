13322af7b919afc7a88fbf7d13fd79400325c610
rafid <ec22909@qmul.ac.uk>
2024-03-29 14:43:44 +0000

    6th push

--- my-react-app/src/HomePage.js
+++ my-react-app/src/HomePage.js
@@ -1,31 +1,52 @@
 // HomePage.js
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 const HomePage = () => {
+  const [subject, setSubject] = useState("");
+  const [mainText, setMainText] = useState("");
+
+  const handleClearText = () => {
+    setSubject("");
+    setMainText("");
+  };
+
   return (
     <div>
       <div className="header">
         <div className="allTextHeader">
-        <p className="Title">HappyHounds</p>
+          <p className="Title">HappyHounds</p>
 
-        <Link to="/" className="Home-page">
-          HOME
-        </Link>
-        <Link to="/profile" className="profile">
-          PROFILE
-        </Link>
-        <Link to="/login" className="login">
-          LOGIN
-        </Link>
+          <Link to="/" className="Home-page">
+            HOME
+          </Link>
+          <Link to="/profile" className="profile">
+            PROFILE
+          </Link>
+          <Link to="/login" className="login">
+            LOGIN
+          </Link>
         </div>
-
       </div>
       <div className="main-content">
         <div className="LeftBox">
-          <input type="text" placeholder="Enter your subject" className="Text-Box-subject" />
-          <textarea type="text" placeholder="Enter your subject" className="Text-Box-main" />
+          <p className="MessageTitle">Send us a Message</p>
+          <input
+            type="text"
+            placeholder="Enter your subject"
+            className="Text-Box-subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+          <textarea
+            type="text"
+            placeholder="Enter your subject"
+            className="Text-Box-main"
+            value={mainText}
+            onChange={(e) => setMainText(e.target.value)}
+          />
+          <button onClick={handleClearText}>Clear Text</button>
         </div>
         <div className="RightBox">
           <p>bruh</p>
