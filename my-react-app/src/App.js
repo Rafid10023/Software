0fafdc7dd092baa936b636a539e6dab48018737b
kenza1605 <162992854+kenza1605@users.noreply.github.com>
2024-04-02 12:22:50 +0100

    Revert "Login and sign up pages"
    This reverts commit 174f67a223ebee3a83ab678dbe3df93639f855e2.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Users from './LoginPages/Users';
-import LoginDogOwner from './LoginPages/LoginDogOwner';
-import LoginDowWalker from './LoginPages/LoginDogWalker';
-import LoginAdmin from './LoginPages/LoginAdmin';
-import SignUp from './LoginPages/SignUp';
+
 function App() {
   return (
-    <div>
-    <BrowserRouter>
-    <Routes>
-      <Route index element = {<Users/>}/>
-      <Route path='/loginOwner' element={<LoginDogOwner/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/loginWalker' element={<LoginDowWalker/>}/>
-      <Route path= '/loginAdmin' element={<LoginAdmin/>}/>
-    </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
