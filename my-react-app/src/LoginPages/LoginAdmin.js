246de9cb044e55bb1cfbfcb8574d824610284d75
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-04-10 01:35:51 +0100

    LoginAdmin

--- my-react-app/src/LoginPages/LoginAdmin.js
+++ my-react-app/src/LoginPages/LoginAdmin.js
@@ -1,49 +1,57 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
+import SystemAdmin from '../SystemAdmin/SystemAdmin.js';
 
 function LoginAdmin() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State to track login status
 
-  const handleLogin = async () => {
-    const response = await fetch('/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    });
-    const data = await response.json();
-    if (response.ok) {
-      console.log("Login successful");
-    } else {
-      console.log("Login unsuccessful");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/admin-login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+      if (response.ok) {
+        console.log('Login successful');
+        setIsLoggedIn(true);
+      } else {
+        console.log('Incorrect username or password');
+      }
+    } catch (error) {
+      console.error('Error:', error);
     }
-  }
+  };
 
-  return (
-    <div className="container">
-      <div className="login-box">
-        <h1>HappyHounds</h1>
-        <p>Welcome back!</p>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+  return isLoggedIn ? <SystemAdmin /> : (
+    <form onSubmit={handleSubmit}>
+      <div className="container">
+        <div className="login-box">
+          <h1>Happy Hounds</h1>
+          <p>Welcome back!</p>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type='submit'>LOGIN</button>
         </div>
-        <button onClick={handleLogin}>LOGIN</button>
-        <p>New Here? <a href="/signup">Sign up</a></p>
       </div>
-    </div>
+    </form>
   );
 }
 
